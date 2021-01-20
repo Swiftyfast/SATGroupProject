259e68c6a520f1e1aea3b0aee4deda75a81d5b2d
Swiftyfast <jdavidswift@gmail.com>
2021-01-20 12:51:00 -0600

    Finished Metadata

--- SATGroupProject.DATA.EF/Metadata/SATGroupProjectMetadata.cs
+++ SATGroupProject.DATA.EF/Metadata/SATGroupProjectMetadata.cs
@@ -8,7 +8,7 @@ using System.ComponentModel.DataAnnotations;
 namespace SATGroupProject.DATA.EF//.Metadata
 {
     #region Enrollment
-    class EnrollmentMetadata
+    public class EnrollmentMetadata
     {
         public int EnrollmentId { get; set; }
         public int StudentId { get; set; }
@@ -24,15 +24,78 @@ namespace SATGroupProject.DATA.EF//.Metadata
     #endregion
 
     #region Course
-
-
-    class CourseMetadata
+    public class CourseMetadata
     {
-
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+        public string CourseDescription { get; set; }
+        public byte CreditHours { get; set; }
+        public string Curriculum { get; set; }
+        public string Notes { get; set; }
+        public bool IsActive { get; set; }
     }
 
     [MetadataType(typeof(CourseMetadata))]
     public partial class Course { }
     #endregion
 
+    #region Student
+    public class StudentMetadata
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Major { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string PhotoUrl { get; set; }
+        public int SSID { get; set; }
+    }
+
+    [MetadataType(typeof(StudentMetadata))]
+    public partial class Student { }
+    #endregion
+
+    #region StudentStatus
+    public class StudentStatusMetadata
+    {
+        public int SSID { get; set; }
+        public string SSName { get; set; }
+        public string SSDescription { get; set; }
+    }
+
+    [MetadataType(typeof(StudentStatusMetadata))]
+    public partial class StudentStatus { }
+    #endregion
+
+    #region ScheduledClass
+    public class ScheduledClassMetadata
+    {
+        public int ScheduledClassId { get; set; }
+        public int CourseId { get; set; }
+        public System.DateTime StartDate { get; set; }
+        public System.DateTime EndDate { get; set; }
+        public string InstructorName { get; set; }
+        public string Location { get; set; }
+        public int SCSID { get; set; }
+    }
+
+    [MetadataType(typeof(ScheduledClassMetadata))]
+    public partial class ScheduledClass { }
+    #endregion
+
+    #region ScheduledClassStatus
+    public class ScheduledClassStatusMetadata
+    {
+        public int SCSID { get; set; }
+        public string SCSName { get; set; }
+    }
+
+    [MetadataType(typeof(ScheduledClassStatusMetadata))]
+    public partial class ScheduledClassStatus { }
+    #endregion
 }
