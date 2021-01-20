75646503cd40940c82d026a81f5617d2fcd90b32
Swiftyfast <jdavidswift@gmail.com>
2021-01-20 11:29:49 -0600

    lunch break commit

---
+++ SATGroupProject.DATA.EF/Metadata/SATGroupProjectMetadata.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SATGroupProject.DATA.EF//.Metadata
+{
+    #region Enrollment
+    class EnrollmentMetadata
+    {
+        public int EnrollmentId { get; set; }
+        public int StudentId { get; set; }
+        public int ScheduledClassId { get; set; }
+        public System.DateTime EnrollmentDate { get; set; }
+
+        public virtual ScheduledClass ScheduledClass { get; set; }
+        public virtual Student Student { get; set; }
+    }
+
+    [MetadataType(typeof(EnrollmentMetadata))]
+    public partial class Enrollment { }
+    #endregion
+
+    #region Course
+
+
+    class CourseMetadata
+    {
+
+    }
+
+    [MetadataType(typeof(CourseMetadata))]
+    public partial class Course { }
+    #endregion
+
+}
